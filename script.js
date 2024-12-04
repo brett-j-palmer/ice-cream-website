260bc40e089ea9c696bbf38b2aa7bf830b14bc26
Brett Palmer <brett.palmer@maine.edu>
2024-12-04 13:30:22 -0500

    navbar slider implemented

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function showSection(sectionName) {    
-  document.querySelectorAll('.section').forEach (
-    sectionDiv => {
-      sectionDiv.style.display = "flex";
-    }
-  )
- document.querySelector(sectionName).style.display = "flex";
- 
+function showSection(sectionName) {
   document.querySelectorAll('.navbar p').forEach(navItem => {
     navItem.style.flex = "1";
     navItem.style.fontSize = "1.5em";
@@ -14,39 +7,14 @@ function showSection(sectionName) {
   const clickedItem = document.querySelector(`#${sectionName.slice(1)}-nav`);
   clickedItem.style.flex = "1.25";
   clickedItem.style.fontSize = '1.75em';
+
+  const sectionOffsets = {
+    ".take-orders": 0,
+    ".make-orders": 1,
+    ".cash-orders": 2
+  };
+
+  const targetOffset = sectionOffsets[sectionName];
   
+  document.querySelector(".frieze").style.transform = `translateX(${targetOffset * -33.333}%)`;
 }
-
-// function loadTake() {
-//   fetch("take-orders.html")
-//   .then(
-//     response => response.text()
-//   )
-//   .then(
-//     text => document.querySelector(".take-orders").innerHTML = text
-//   )
-//   showSection('.take-orders');
-// }
-
-// function loadMake() {
-//   fetch("make-orders.html")
-//   .then(
-//     response => response.text()
-//   )
-//   .then(
-//     text => document.querySelector(".order-checklist").innerHTML = text
-//   )
-//   showSection('.make-orders');
-// }
-
-// function loadCash() {
-//   fetch("cash-orders.html")
-//   .then(
-//     response => response.text()
-//   )
-//   .then(
-//     text => document.querySelector(".order-totals").innerHTML = text
-//   )
-//   showSection('.cash-orders');
-// }
-
